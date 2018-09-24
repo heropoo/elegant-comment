908eb66a1f5b26d2d73d15ccb86061f0a9f3f4ab
heropoo <aiyouyou1000@163.com>
2018-09-25 00:07:58 +0800

    login

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ttt
+ * Date: 2018/9/24
+ * Time: 23:53
+ */
+
+namespace App\Controllers;
+
+
+use Moon\Controller;
+
+class UserController extends Controller
+{
+    public function login(){
+        //todo
+    }
+}
