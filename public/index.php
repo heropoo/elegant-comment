a4d0c73d6e9953ad12eee93cdc4231b569d64302
heropoo <aiyouyou1000@163.com>
2020-05-08 15:30:40 +0800

    跨域

--- public/index.php
+++ public/index.php
@@ -20,11 +20,11 @@ if (in_array($origin, $origins)) {
     // 允许 $originarr 数组内的 域名跨域访问
     header('Access-Control-Allow-Origin:' . $origin);
     // 响应类型
-    header('Access-Control-Allow-Methods:POST,GET');
+//    header('Access-Control-Allow-Methods: *');
     // 带 cookie 的跨域访问
-    header('Access-Control-Allow-Credentials: true');
+//    header('Access-Control-Allow-Credentials: true');
     // 响应头设置
-    header('Access-Control-Allow-Headers:x-requested-with,Content-Type,App-Id,App-Key');
+//    header('Access-Control-Allow-Headers:x-requested-with,Content-Type,App-Id,App-Key');
 }
 
 require __DIR__ . '/../vendor/autoload.php';
