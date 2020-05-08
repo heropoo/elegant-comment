522461e51aa3e39aaa2a20a310fcad75c1dd270a
heropoo <aiyouyou1000@163.com>
2020-05-08 13:00:47 +0800

    header

--- public/index.php
+++ public/index.php
@@ -5,16 +5,25 @@
  * @datetime 2018-09-20 18:25
  */
 
+$origins = [
+    'https://metmoon.com',
+    'http://metmoon.com',
+    'http://127.0.0.1:4000',
+];
+
 // 获取当前跨域域名
 $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-// 允许 $originarr 数组内的 域名跨域访问
-header('Access-Control-Allow-Origin:' . $origin);
-// 响应类型
-header('Access-Control-Allow-Methods:POST,GET');
-// 带 cookie 的跨域访问
-header('Access-Control-Allow-Credentials: true');
-// 响应头设置
-header('Access-Control-Allow-Headers:x-requested-with,Content-Type,App-Id,App-Key');
+if (in_array($origin, $origins)) {
+    // 允许 $originarr 数组内的 域名跨域访问
+    header('Access-Control-Allow-Origin:' . $origin);
+    // 响应类型
+    header('Access-Control-Allow-Methods:POST,GET');
+    // 带 cookie 的跨域访问
+    header('Access-Control-Allow-Credentials: true');
+    // 响应头设置
+    header('Access-Control-Allow-Headers:x-requested-with,Content-Type,App-Id,App-Key');
+}
+
 
 require __DIR__ . '/../vendor/autoload.php';
 $app = new \Moon\Application(dirname(__DIR__));
